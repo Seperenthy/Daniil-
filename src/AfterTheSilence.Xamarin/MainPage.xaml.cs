df535bb3c32f5b00bdc696d5479e534433ddaa9f
limeniye <limeniye@gmail.com>
2022-07-24 20:38:02 +0300

    - Added Xamarin project for Android;

---
+++ src/AfterTheSilence.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AfterTheSilence.Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
