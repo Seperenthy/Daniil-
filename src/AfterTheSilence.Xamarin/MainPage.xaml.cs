7937792ed7ab839c945aa5535526d39b7564b4a8
limeniye <limeniye@gmail.com>
2022-07-25 00:46:09 +0300

    - Created ChatViewModel;

--- src/AfterTheSilence.Xamarin/MainPage.xaml.cs
+++ src/AfterTheSilence.Xamarin/MainPage.xaml.cs
@@ -1,35 +1,24 @@
 ﻿using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
 using System.Reactive;
-using System.Reactive.Concurrency;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace AfterTheSilence.Xamarin
 {
-    public partial class MainPage : ContentPage
+    public class ChatViewModel : ReactiveObject
     {
-        private readonly List<MessageBlock> messageBlocks = new List<MessageBlock>();
-        private readonly IFordePathService _fordePathService;
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set => this.RaiseAndSetIfChanged(ref _message, value);
+        }
 
         public ObservableCollection<Message> Messages { get; } = new ObservableCollection<Message>()
         {
-            new Message(MessageType.Owner, "Овнер", "owner", DateTime.Now),
-            new Message(MessageType.Bot, "Bot", "bot", DateTime.Now)
+            new Message(MessageType.Bot, ":)", "bot", DateTime.Now)
         };
-
-
-        [Reactive]
-        public string Message { get; set; }
-
-        #region SendMessageCommand
 
         public ReactiveCommand<Unit, Unit> SendMessageCommand =>
             ReactiveCommand.Create(OnSendMessageExecute);
@@ -39,130 +28,21 @@ namespace AfterTheSilence.Xamarin
             if (string.IsNullOrWhiteSpace(Message))
                 return;
 
-            var message = Message;
-            Message = string.Empty;
-            var newMessage = new Message( MessageType.Owner, message, "owner", DateTime.UtcNow);
-            Messages.Add(newMessage);
+            string message = Message;
+            Message = String.Empty;
+            Messages.Add(new Message(MessageType.Owner, message, "owner", DateTime.UtcNow));
+
+            string response = BotService.GetResponse(message);
+            Messages.Add(new Message(MessageType.Bot, response, "bot", DateTime.UtcNow));
         }
+    }
 
-        #endregion
-
-
-        public MainPage(IFordePathService folderPath)
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
         {
-            _fordePathService = folderPath;
-
             InitializeComponent();
-
-            InitializeScript();
-
-            BindingContext = this;
+            BindingContext = new ChatViewModel();
         }
-
-        private async void InitializeScript()
-        {
-            while (await Permissions.CheckStatusAsync<Permissions.StorageRead>() == PermissionStatus.Denied)
-            {
-                await Permissions.RequestAsync<Permissions.StorageRead>();
-                await Task.Delay(100);
-            }
-            while (await Permissions.CheckStatusAsync<Permissions.StorageWrite>() == PermissionStatus.Denied)
-            {
-                await Permissions.RequestAsync<Permissions.StorageWrite>();
-                await Task.Delay(100);
-            }
-
-            string[] lines = ReadAllLines();
-            MessageBlock currentMessageBlock = new MessageBlock();
-            bool isFirst = true, isAnother = false;
-            MessageTag lastTag = MessageTag.Empty;
-
-            foreach (var line in lines)
-            {
-                if (line.Length > 0 && line[0] is '_')
-                {
-                    if (!isFirst)
-                    {
-                        messageBlocks.Add(currentMessageBlock);
-                        currentMessageBlock = new MessageBlock();
-                    }
-                    else
-                    {
-                        isFirst = false;
-                    }
-                    continue;
-                }
-
-                string tag = new string(line.Take(2).ToArray());
-                string text = string.Empty;
-
-                isAnother = true;
-
-                switch (tag)
-                {
-                    case "m>":
-                        text = line.Substring(2, line.Length - 2);
-                        lastTag = MessageTag.Message;
-                        break;
-                    case "r>":
-                        text = line.Substring(2, line.Length - 2);
-                        lastTag = MessageTag.Response;
-                        break;
-                    default:
-                        isAnother = false;
-                        break;
-                }
-
-                if (isAnother && lastTag == MessageTag.Message)
-                {
-                    currentMessageBlock.Messages.Add(text);
-                    continue;
-                }
-
-                if (!isAnother && lastTag == MessageTag.Message)
-                {
-                    int lastIndex = currentMessageBlock.Messages.Count - 1;
-                    string lastText = currentMessageBlock.Messages[lastIndex];
-                    currentMessageBlock.Messages[lastIndex] = lastText + Environment.NewLine + line;
-                    continue;
-                }
-
-                if (isAnother && lastTag == MessageTag.Response)
-                {
-                    currentMessageBlock.Responses.Add(text);
-                    continue;
-                }
-
-                if (!isAnother && lastTag == MessageTag.Response)
-                {
-                    int lastIndex = currentMessageBlock.Responses.Count - 1;
-                    string lastText = currentMessageBlock.Responses[lastIndex];
-                    currentMessageBlock.Responses[lastIndex] = lastText + Environment.NewLine + line;
-                    continue;
-                }
-            }
-        }
-
-        private string[] ReadAllLines()
-        {
-            var backingFile = Path.Combine(_fordePathService.GetMainFordePath(), "script.txt");
-
-            if (backingFile == null || !File.Exists(backingFile))
-                File.Create(backingFile).Close();
-
-            return File.ReadAllLines(backingFile);
-        }
-
-        /*
-            m>Чего мне нужно больше?
-            r>Золота
-            r>Построить зиккурат
-            ___________
-            m>Привет!
-            m>ку
-            r>Извини, я занят, скребу крышку гроба.
-            Отпишу позже.
-            ___________
-         */
     }
 }
